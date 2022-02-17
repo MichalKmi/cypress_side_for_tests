1b95a324707baf13b5251d010df131ead571166e
MichalKmi <michal.kmi@gmail.com>
2022-02-17 15:09:50 +0100

    Secrets moved to secret place :)

--- cypress/integration/apitests/getuser.spec.js
+++ cypress/integration/apitests/getuser.spec.js
@@ -1,16 +1,17 @@
-const { describe } = require("yargs");
-
 describe('get api user tests', () => {
 
     it('get users', () => {
-        // cy.request({
-        //     method: 'GET',
-        //     url: 'https://gorest.co.in/public/v2/users',
-        //     headers: {
-        //         Authorization: 'Bearer 3421caf8553804c839d42121ac50ab9bd42951ccab3f9e538e3606f31d3fbe89'
-        //     }
-        // }).then((response) => {
-        //     console.log(response);
-        // })
+        cy.log(Cypress.env("GOREST_BEARER"))
+        cy.request({
+            method: 'GET',
+            url: 'https://gorest.co.in/public/v2/users/4906',
+            headers: {
+                Authorization: Cypress.env("GOREST_BEARER")
+            }
+        }).then((response) => {
+            cy.log(JSON.stringify(response.body));
+            cy.expects(response.status).equal(200);
+            cy.expect(JSON.stringify(response.body.gender)).to.equal('"male"');
+        })
     })
 })
