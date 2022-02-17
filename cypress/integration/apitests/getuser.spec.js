5bc9a6fe7647c92b15f871655c43d562ceca5570
MichalKmi <michal.kmi@gmail.com>
2022-02-17 14:14:50 +0100

    Dependencies update

---
+++ cypress/integration/apitests/getuser.spec.js
@@ -0,0 +1,16 @@
+const { describe } = require("yargs");
+
+describe('get api user tests', () => {
+
+    it('get users', () => {
+        // cy.request({
+        //     method: 'GET',
+        //     url: 'https://gorest.co.in/public/v2/users',
+        //     headers: {
+        //         Authorization: 'Bearer 3421caf8553804c839d42121ac50ab9bd42951ccab3f9e538e3606f31d3fbe89'
+        //     }
+        // }).then((response) => {
+        //     console.log(response);
+        // })
+    })
+})
